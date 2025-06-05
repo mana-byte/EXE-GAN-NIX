de884f0e737ab5ecb290255b1fe9550cf1f30370
mana-byte <manaikilaut@gmail.com>
2025-06-05 14:46:13 +1200

    added video output to guided recovery

--- guided_recovery.py
+++ guided_recovery.py
@@ -1,21 +1,25 @@
 # -*- coding: UTF-8 -*-
-#coding=utf-8
+# coding=utf-8
 import sys
+
 # sys.path.append('./')
 # print(sys.path)
-#guided facial image recovery
+# guided facial image recovery
 import argparse
 import math
 import random
 import os
 from torch import nn, autograd
 from torch.utils import data
-from torchvision import  utils
+from torchvision import utils
 from tqdm import tqdm
-from op.utils import mkdirs,delete_dirs,set_random_seed
+from op.utils import mkdirs, delete_dirs, set_random_seed
+
+import cv2
 
 from img_load_util import *
 from test import get_model
+
 try:
     import wandb
 
@@ -55,8 +59,6 @@ def sample_data(loader):
             yield batch
 
 
-
-
 def d_logistic_loss(real_pred, fake_pred):
     real_loss = F.softplus(-real_pred)
     fake_loss = F.softplus(fake_pred)
@@ -64,10 +66,9 @@ def d_logistic_loss(real_pred, fake_pred):
     return real_loss.mean() + fake_loss.mean()
 
 
-
 def d_r1_loss(real_pred, real_img):
     with conv2d_gradfix.no_weight_gradients():
-        grad_real, = autograd.grad(
+        (grad_real,) = autograd.grad(
             outputs=real_pred.sum(), inputs=real_img, create_graph=True
         )
     grad_penalty = grad_real.pow(2).reshape(grad_real.shape[0], -1).sum(1).mean()
@@ -80,11 +81,12 @@ def g_nonsaturating_loss(fake_pred):
 
     return loss
 
+
 def g_path_regularize(fake_img, latents, mean_path_length, decay=0.01):
     noise = torch.randn_like(fake_img) / math.sqrt(
         fake_img.shape[2] * fake_img.shape[3]
     )
-    grad, = autograd.grad(
+    (grad,) = autograd.grad(
         outputs=(fake_img * noise).sum(), inputs=latents, create_graph=True
     )
     path_lengths = torch.sqrt(grad.pow(2).sum(2).mean(1))
@@ -118,21 +120,23 @@ def set_grad_none(model, targets):
         if n in targets:
             p.grad = None
 
-def get_random_index(tar_size,part_size):
-    range_ = int(tar_size/part_size)
 
-    index_y = random.randint(0,range_-1)
-    index_x = random.randint(0,range_-1)
+def get_random_index(tar_size, part_size):
+    range_ = int(tar_size / part_size)
 
-    return index_y,index_x
+    index_y = random.randint(0, range_ - 1)
+    index_x = random.randint(0, range_ - 1)
 
-def crop_image_by_part(image, ind_y,ind_x,part_size):
-    y_start = ind_y*part_size
-    x_satrt = ind_x*part_size
-    return image[:, :, y_start:y_start+part_size,x_satrt:x_satrt +part_size]
+    return index_y, index_x
 
 
-def get_mix_latent(infer_img,psp_embedding,lambda_=None):
+def crop_image_by_part(image, ind_y, ind_x, part_size):
+    y_start = ind_y * part_size
+    x_satrt = ind_x * part_size
+    return image[:, :, y_start : y_start + part_size, x_satrt : x_satrt + part_size]
+
+
+def get_mix_latent(infer_img, psp_embedding, lambda_=None):
     noise = mixing_noise(args.batch, args.latent, args.mixing, device)
     w_style = psp_embedding.get_style_mapping(noise)
     infer_embedding = psp_embedding.get_w_plus(infer_img)
@@ -143,12 +147,12 @@ def get_mix_latent(infer_img,psp_embedding,lambda_=None):
     return infer_embedding
 
 
-
 def get_name_fromTime():
     import time
+
     time.sleep(1)
     time_now = int(round(time.time() * 1000))
-    time_now = time.strftime('%Y%m%d%H%M%S', time.localtime(time_now / 1000))
+    time_now = time.strftime("%Y%m%d%H%M%S", time.localtime(time_now / 1000))
     return time_now
 
 
@@ -156,20 +160,62 @@ if __name__ == "__main__":
     device = "cuda"
 
     parser = argparse.ArgumentParser(description="EXE-GAN guided facial image recovery")
-    parser.add_argument('--arch', type=str, default='exe_gan', help='models architectures ( exe_gan)')
+    parser.add_argument(
+        "--arch", type=str, default="exe_gan", help="models architectures ( exe_gan)"
+    )
     parser.add_argument("--batch", type=int, default=1, help="batch sizes for each gpu")
-    parser.add_argument("--size", type=int, default=256, help="image sizes for the models")
-    parser.add_argument("--psp_checkpoint_path", type=str, default="./pre-train/psp_ffhq_encode.pt",help="psp model pretrained model")
-    parser.add_argument("--mixing", type=float, default=0.5, help="probability of latent code mixing")
-    parser.add_argument("--ckpt", type=str, default="./checkpoint/EXE_GAN_model.pt", help="psp model pretrained model")
+    parser.add_argument(
+        "--size", type=int, default=256, help="image sizes for the models"
+    )
+    parser.add_argument(
+        "--psp_checkpoint_path",
+        type=str,
+        default="./pre-train/psp_ffhq_encode.pt",
+        help="psp model pretrained model",
+    )
+    parser.add_argument(
+        "--mixing", type=float, default=0.5, help="probability of latent code mixing"
+    )
+    parser.add_argument(
+        "--ckpt",
+        type=str,
+        default="./checkpoint/EXE_GAN_model.pt",
+        help="psp model pretrained model",
+    )
 
-    parser.add_argument("--masked_dir", type=str, default="./imgs/exe_guided_recovery/mask", help=" the input  masked_dir ")
-    parser.add_argument("--gt_dir", type=str, default="./imgs/exe_guided_recovery/target", help="the input gt_dir, used for  editing ")
-    parser.add_argument("--exemplar_dir", type=str, default="./imgs/exe_guided_recovery/exemplar", help="exemplar_dir, the exemplar dir, for guiding the editing")
-    parser.add_argument("--sample_times", type=int, default=10, help="image sizes for the models")
-    parser.add_argument("--eval_dir", type=str, default="./recover_out", help="path to the output the generated images")
-
-
+    parser.add_argument(
+        "--masked_dir",
+        type=str,
+        default="./imgs/exe_guided_recovery/mask",
+        help=" the input  masked_dir ",
+    )
+    parser.add_argument(
+        "--gt_dir",
+        type=str,
+        default="./imgs/exe_guided_recovery/target",
+        help="the input gt_dir, used for  editing ",
+    )
+    parser.add_argument(
+        "--exemplar_dir",
+        type=str,
+        default="./imgs/exe_guided_recovery/exemplar",
+        help="exemplar_dir, the exemplar dir, for guiding the editing",
+    )
+    parser.add_argument(
+        "--sample_times", type=int, default=10, help="image sizes for the models"
+    )
+    parser.add_argument(
+        "--eval_dir",
+        type=str,
+        default="./recover_out",
+        help="path to the output the generated images",
+    )
+    parser.add_argument(
+        "--video_output",
+        type=str,
+        default="",
+        help="path to the output video",
+    )
 
     args = parser.parse_args()
 
@@ -186,22 +232,46 @@ if __name__ == "__main__":
     mask_imgs = get_img_lists(args.masked_dir, mask_post)
     exe_imgs = get_img_lists(args.exemplar_dir, exe_post)
 
-
     generator = get_model(args, model_path=args.ckpt, psp_path=args.psp_checkpoint_path)
 
-    for i in tqdm(range(len(exe_imgs))):
-        exe_img_ = load_img2tensor(exe_imgs[i],args.size).to(device)
+    if args.video_output != "":
+        output_video = cv2.VideoWriter(
+            args.video_output,
+            cv2.VideoWriter_fourcc(*"mp4v"),
+            30,
+            (args.size, args.size),
+        )
+
+    for i in tqdm(range(len(gt_imgs))):
+        exe_img_ = load_img2tensor(exe_imgs[0], args.size).to(device)
         gt_img_ = load_img2tensor(gt_imgs[i], args.size).to(device)
         mask_ = load_mask2tensor(mask_imgs[i], args.size).to(device)
 
         ##get mask
         mask_01 = mask_
-        name = str(os.path.basename(exe_imgs[i])).split("_")[0]
+        name = str(os.path.basename(gt_imgs[i])).split("_")[0]
         for jj in range(args.sample_times):
-            completed_img, _, infer_imgs,_ = generator.get_inherent_stoc(gt_img_, mask_01,infer_imgs=exe_img_)
+            completed_img, _, infer_imgs, _ = generator.get_inherent_stoc(
+                gt_img_, mask_01, infer_imgs=exe_img_
+            )
 
             utils.save_image(
                 completed_img.add(1).mul(0.5),
                 f"{str(args.eval_dir)}/{name}_{jj}_inpaint.png",
                 nrow=int(1),
             )
+            if args.video_output != "":
+                frame = (
+                    completed_img.add(1)
+                    .mul(0.5)
+                    .squeeze(0)
+                    .permute(1, 2, 0)
+                    .cpu()
+                    .detach()
+                    .numpy()
+                )
+                frame = (frame * 255).astype("uint8")
+                frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+                output_video.write(frame)
+    if args.video_output != "":
+        output_video.release()
